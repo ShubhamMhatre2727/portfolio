b4af29ff3b6382807aec0f29e172d56f0f37bef4
ShubhamMhatre2727 <shubhammhatre2727@gmail.com>
2024-07-28 19:11:13 +0530

    done with home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,22 +3,24 @@ import React from 'react';
 export const Home = () => {
   return (
     <div className='Home'>
-        <div className='h-[90%] flex flex-col justify-center items-center'>
-            <h1 className='text-[42px] text-white font-semibold'>Shubham Mhatre</h1>
-            <p className='md:w-[60%] text-gray-100 text-center text-sm font-semibold mx-2'>I’m Web Developer from Panvel graduated in ‘Information Technology’ from saraswati college of engineering, kharghar.</p>
-            <div className='flex gap-2 text-white text-xs  tracking-widest font-semibold mt-4'>
-                <button className='bg-white bg-opacity-25 rounded-sm px-4 py-1 border border-transparent hover:border-white'>
-                    <h1 className='text-white'>See My Work</h1>
+        <div className='h-[90%] flex flex-col md:items-start justify-center items-center px-10'>
+            <h1 className='sm:text-[60px] text-[42px] text-white font-semibold'>Shubham Mhatre</h1>
+            <p className='sm:w-[55%] w-80 sm:text-lg md:text-start text-center text-sm'>I’m Web Developer from Panvel graduated in ‘Information Technology’ from saraswati college of engineering, kharghar.</p>
+            <div className='flex gap-2 text-white text-xs md:text-sm  tracking-widest font-semibold mt-4'>
+                <button className='bg-white bg-opacity-25 rounded-sm px-4 py-1 border-2 border-transparent hover:border-white'>
+                    <h1 className="text-white">See My Work</h1>
                 </button>
-                <button className='bg-purple-800 bg-opacity-65 rounded-sm px-8 py-1 border border-transparent hover:border-white'>
+                <button className='bg-purple-800 bg-opacity-65 rounded-sm px-8 py-1 border-2 border-transparent hover:border-white'>
                     <h1 className='text-white'>Resume</h1>
                 </button>
             </div>
         </div>
-        <div className="h-[10%] w-full">
-            <img src="public/right-arrow-svgrepo-com.svg" alt=""/>
-            <h3>LinkedIn</h3>
-            <h3>Instagram</h3>
+        <div className="h-[10%] w-full p-2 flex gap-3 font-semibold cursor-pointer">
+            <div className='sm:w-[90%] w-[65%] flex items-center justify-start text-xs'>
+                <p className='w-full border border-gray-400'/>&gt;
+            </div>
+            <img className='' src="public\images\linkedin-svgrepo-com.svg" alt="" />
+            <img src="public\images\github-svgrepo-com.svg" alt="" />
         </div>
     </div>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const NavBar = () => {
   return (
-    <nav className='absolute list-none w-full flex justify-center gap-5 py-3 cursor-pointer text-sm'>
+    <nav className='absolute list-none w-full flex justify-center gap-10 py-3 cursor-pointer text-sm'>
         <li className='hover:text-white'>Home</li>
         <li className='hover:text-white'>Projects</li>
         <li className='hover:text-white'>Contact</li>
