7d2c60c6a1adfc381ed306420a79db157e866844
ShubhamMhatre2727 <shubhammhatre2727@gmail.com>
2024-08-01 14:26:07 +0530

    added animation to home page and adjusted home-footer position for small devices

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,72 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 import React from 'react';
 
 export const Home = () => {
+
+    useGSAP(()=>{
+        gsap.fromTo(".Home",{
+            filter: "brightness(0%)"
+        },{
+            delay:1,
+            filter: "brightness(100%)",
+        })
+
+        const tl = gsap.timeline();
+
+        tl.fromTo(["h1","p"],{
+            translateX:-600,
+        },{
+            translateX:0,
+            delay:1,
+            stagger:0.1
+        })
+
+        // hr tag animation start at same time as h1
+        tl.fromTo("hr",{
+            width:0,
+        },{
+            width:"100%",
+            duration:2
+        },"<")
+
+        tl.fromTo("#home-footer > img",{
+            opacity:0,
+        },{
+            opacity:1
+        })
+
+        tl.fromTo("a",{
+            rotateX:90,
+            translateY:-14,
+        },{
+            rotateX:0,
+            translateY:0,
+            ease: "bounce.out",
+            stagger:0.1
+        })
+        
+    },[])
+
   return (
     <div className='Home'>
         <div className='h-[90%] flex flex-col md:items-start justify-center items-center px-10'>
-            <h1 className='sm:text-6xl text-4xl text-white font-semibold'>Shubham Mhatre</h1>
-            <p className='md:w-[40%] sm:w-96 w-80 sm:text-lg md:text-start text-center text-sm'>I’m Web Developer from Panvel graduated in ‘Information Technology’ from saraswati college of engineering, kharghar.</p>
-            <div className='flex gap-2 text-white text-xs md:text-sm  tracking-widest font-semibold mt-4'>
+            <h1 id='heading' className='sm:text-6xl text-4xl text-white font-semibold'>Shubham Mhatre</h1>
+            <p id='description' className='md:w-[40%] sm:w-96 w-80 sm:text-lg md:text-start text-center text-sm'>I’m Web Developer from Panvel graduated in ‘Information Technology’ from saraswati college of engineering, kharghar.</p>
+            <div className=' flex gap-2 text-white text-xs md:text-sm  tracking-widest font-semibold mt-4'>
                 <a href='#Projects' className='bg-white bg-opacity-25 rounded-md px-4 py-1 border-2 border-transparent hover:border-gray-300'>
-                    <h1 className="text-white">See My Work</h1>
+                    See My Work
                 </a>
-                <button className='bg-purple-800 bg-opacity-65 rounded-md px-8 py-1 border-2 border-transparent hover:border-gray-300'>
-                    <h1 className='text-white'>Resume</h1>
-                </button>
+                <a className='bg-purple-800 bg-opacity-65 rounded-md px-8 py-1 border-2 border-transparent hover:border-gray-300'>
+                    Resume
+                </a>
             </div>
         </div>
-        <div className="h-[10%] w-full md:px-10 px-4 flex gap-3 font-semibold cursor-pointer">
+        <div id="home-footer" className="h-[10%] w-full md:px-10 px-4 flex gap-3 font-semibold -translate-y-20 sm:translate-y-0 cursor-pointer">
             <div className='sm:w-[90%] w-[65%] flex items-center justify-start text-xs'>
-                <p className='w-full border border-gray-400'/>&gt;
+                <hr className='w-full border border-gray-400'/>&gt;
             </div>
-            <img className='' src="\images\linkedin-svgrepo-com.svg" alt="" />
+            <img src="\images\linkedin-svgrepo-com.svg" alt="" />
             <img src="\images\github-svgrepo-com.svg" alt="" />
         </div>
     </div>
